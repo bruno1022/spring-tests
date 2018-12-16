5d7429d83c6a1e168f9482d1e465f87ef94ddcc9
bruno1022 <40252090+bruno1022@users.noreply.github.com>
2018-12-16 15:30:48 -0200

    first commit

---
+++ src/main/java/com/javaee/bruno/springtests/SpringTestsApplication.java
@@ -0,0 +1,14 @@
+package com.javaee.bruno.springtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestsApplication.class, args);
+	}
+
+}
+
